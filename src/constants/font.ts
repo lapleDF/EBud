0f35151da29ed3ce1140270e2e838dea9a7326fe
lapleDF <laple@digitalfortress.dev>
2023-05-08 17:38:55 +0700

    Add Neuton font, Develop 3 onboading screens

--- src/constants/font.ts
+++ src/constants/font.ts
@@ -1,5 +1,10 @@
 export const FONTS = {
+  // Poppins
   PoppinsRegular: 'Poppins-Regular',
   PoppinsBold: 'Poppins-Bold',
   PoppinsItalic: 'Poppins-Italic',
+  // Neuton
+  NeutonRegular: 'Neuton-Regular',
+  NeutonBold: 'Neuton-Bold',
+  NeutonItalic: 'Neuton-Italic',
 };
--- src/screens/Onboarding.tsx
+++ src/screens/Onboarding.tsx
@@ -1,13 +1,27 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useNavigation} from '@react-navigation/native';
 import React, {useState, useEffect} from 'react';
-import {Image} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
 import BottomTabNavigator from '../navigators/BottomTabNavigator';
+import CSText from '../components/core/CSText';
+import CSContainer from '../components/core/CSContainer';
+import {ONBOARDING} from '../constants/onBoarding.constant';
+
+const SplashScreen = () => {
+  return (
+    <CSContainer style={styles.container}>
+      <Image
+        source={require('../assets/images/logo.png')}
+        style={styles.image}
+      />
+    </CSContainer>
+  );
+};
 
 const OnboardingScreen = () => {
   const [isFirstLaunch, setIsFirstLaunch] = useState<any>(null);
-  const navigation = useNavigation();
+  const navigation = useNavigation<any>();
 
   useEffect(() => {
     AsyncStorage.getItem('alreadyLaunched').then(value => {
@@ -20,7 +34,7 @@ const OnboardingScreen = () => {
   }, []);
 
   if (isFirstLaunch === null) {
-    return null;
+    return <SplashScreen />;
   } else if (isFirstLaunch === true) {
     return (
       <Onboarding
@@ -32,31 +46,55 @@ const OnboardingScreen = () => {
           AsyncStorage.setItem('alreadyLaunched', 'true');
           navigation.navigate('bottomTab');
         }}
-        pages={[
-          {
-            backgroundColor: '#fff',
-            image: <Image source={require('../assets/images/developer.png')} />,
-            title: 'Welcome to the App',
-            subtitle: 'This is a brief introduction to the app',
-          },
-          {
-            backgroundColor: '#fff',
-            image: <Image source={require('../assets/images/developer.png')} />,
-            title: 'Feature 1',
-            subtitle: 'This is a description of feature 1',
-          },
-          {
-            backgroundColor: '#fff',
-            image: <Image source={require('../assets/images/developer.png')} />,
-            title: 'Feature 2',
-            subtitle: 'This is a description of feature 2',
-          },
-        ]}
+        nextLabel={<CSText color="primaryDark">Tiếp theo</CSText>}
+        skipLabel={<CSText>Bỏ qua</CSText>}
+        controlStatusBar={false}
+        pages={ONBOARDING.map(page => {
+          return {
+            backgroundColor: page.backgroundColor,
+            image: <Image source={page.image} style={styles.image} />,
+            title: (
+              <CSText
+                size={'xxl'}
+                style={styles.text}
+                variant="NeutonBold"
+                color="primaryLight">
+                {page.title}
+              </CSText>
+            ),
+            subtitle: (
+              <CSText
+                size={'xxl'}
+                style={styles.text}
+                variant="NeutonItalic"
+                color="terarity">
+                {page.subtitle}
+              </CSText>
+            ),
+          };
+        })}
       />
     );
   } else {
     return <BottomTabNavigator />;
   }
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    resizeMode: 'cover',
+    height: 250,
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    marginTop: 20,
+    marginHorizontal: 10,
+  },
+});
 
 export default OnboardingScreen;
